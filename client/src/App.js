083fd2c9489a58ece3f78991f86aa1e28c69443e
Waithid <waithit006@gmail.com>
2019-06-20 23:52:43 +0700

    add client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.sass';
+import Navbar from './components/layout/Navbar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+       <Navbar />
+       <section className="hero is-link is-fullheight-with-navbar">
+  <div className="hero-body">
+    <div className="container">
+    <div className="timeline is-centered">
+  <header className="timeline-header">
+    <span className="tag is-medium is-primary">Start</span>
+  </header>
+  <div className="timeline-item is-primary">
+    <div className="timeline-marker is-primary"></div>
+    <div className="timeline-content">
+      <p className="heading">January 2016</p>
+      <p>Timeline content - Can include any HTML element</p>
     </div>
+  </div>
+  <div className="timeline-item is-warning">
+    <div className="timeline-marker is-warning is-image is-32x32">
+      <img src="http://bulma.io/images/placeholders/32x32.png"/>
+    </div>
+    <div className="timeline-content">
+      <p className="heading">February 2016</p>
+      <p>Timeline content - Can include any HTML element</p>
+    </div>
+  </div>
+  <header className="timeline-header">
+    <span className="tag is-primary">2017</span>
+  </header>
+  <div className="timeline-item is-danger">
+    <div className="timeline-marker is-danger is-icon">
+      <i className="fa fa-flag"></i>
+    </div>
+    <div className="timeline-content">
+      <p className="heading">March 2017</p>
+      <p>Timeline content - Can include any HTML element</p>
+    </div>
+  </div>
+  <header className="timeline-header">
+    <span className="tag is-medium is-primary">End</span>
+  </header>
+</div>
+    </div>
+  </div>
+</section>
+  </div>
   );
 }
 
